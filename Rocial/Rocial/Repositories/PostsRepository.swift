6b09bfa9d94564e7645992e7cabd0bb21bc362b6
Roshan Mykoo <roshanjmykoo@gmail.com>
2022-09-12 18:11:20 -0400

    working on adding a favorites tab
    adding a tab to showcase all the posts that a user had liked

--- Rocial/Rocial/Repositories/PostsRepository.swift
+++ Rocial/Rocial/Repositories/PostsRepository.swift
@@ -10,54 +10,56 @@ import Firebase
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 
+// MARK: - PostsRepositoryProtocol
+
 protocol PostsRepositoryProtocol {
-    func fetchPosts() async throws -> [Post]
+    func fetchAllPosts() async throws -> [Post]
+    func fetchFavoritePosts() async throws -> [Post]
     func create(_ post: Post) async throws
     func delete(_ post: Post) async throws
     func favorite(_ post: Post) async throws
     func unfavorite(_ post: Post) async throws
 }
 
+// MARK: - PostsRepositoryStub
+
 #if DEBUG
 struct PostsRepositoryStub: PostsRepositoryProtocol {
-    func favorite(_ post: Post) async throws {}
-    
-    func unfavorite(_ post: Post) async throws {}
-    
-    func delete(_ post: Post) async throws {}
-    
     let state: Loadable<[Post]>
     
-    func fetchPosts() async throws -> [Post] {
+    func fetchAllPosts() async throws -> [Post] {
+        return try await state.simulate()
+    }
+    
+    func fetchFavoritePosts() async throws -> [Post] {
         return try await state.simulate()
     }
     
     func create(_ post: Post) async throws {}
     
+    func delete(_ post: Post) async throws {}
+    
+    func favorite(_ post: Post) async throws {}
+    
+    func unfavorite(_ post: Post) async throws {}
 }
 #endif
 
+// MARK: - PostsRepository
+
 struct PostsRepository: PostsRepositoryProtocol {
-    //let postsReference = Firestore.firestore().collection("posts")
     let postsReference = Firestore.firestore().collection("posts_v1")
     
-    func unfavorite(_ post: Post) async throws {
-        let document = postsReference.document(post.id.uuidString)
-        try await document.setData(["isFavorite" : false], merge: true)
+    func fetchAllPosts() async throws -> [Post] {
+        return try await fetchPosts(from: postsReference)
     }
     
-    func favorite(_ post: Post) async throws {
-        let document = postsReference.document(post.id.uuidString)
-        try await document.setData(["isFavorite" : true], merge: true)
+    func fetchFavoritePosts() async throws -> [Post] {
+        return try await fetchPosts(from: postsReference.whereField("isFavorite", isEqualTo: true))
     }
     
-    func delete(_ post: Post) async throws {
-        let document = postsReference.document(post.id.uuidString)
-        try await document.delete()
-    }
-    
-    func fetchPosts() async throws -> [Post] {
-        let snapshot = try await postsReference
+    private func fetchPosts(from query: Query) async throws -> [Post] {
+        let snapshot = try await query
             .order(by: "timestamp", descending: true)
             .getDocuments()
         return snapshot.documents.compactMap { document in
@@ -69,11 +71,28 @@ struct PostsRepository: PostsRepositoryProtocol {
         let document = postsReference.document(post.id.uuidString)
         try await document.setData(from: post)
     }
+    
+    func delete(_ post: Post) async throws {
+        let document = postsReference.document(post.id.uuidString)
+        try await document.delete()
+    }
+    
+    func favorite(_ post: Post) async throws {
+        let document = postsReference.document(post.id.uuidString)
+        try await document.setData(["isFavorite": true], merge: true)
+    }
+    
+    func unfavorite(_ post: Post) async throws {
+        let document = postsReference.document(post.id.uuidString)
+        try await document.setData(["isFavorite": false], merge: true)
+    }
 }
 
 private extension DocumentReference {
     func setData<T: Encodable>(from value: T) async throws {
-        return try await withCheckedThrowingContinuation( { continuation in
+        return try await withCheckedThrowingContinuation { continuation in
+            // Method only throws if there’s an encoding error, which indicates a problem with our model.
+            // We handled this with a force try, while all other errors are passed to the completion handler.
             try! setData(from: value) { error in
                 if let error = error {
                     continuation.resume(throwing: error)
@@ -81,6 +100,6 @@ private extension DocumentReference {
                 }
                 continuation.resume()
             }
-        })
+        }
     }
 }
