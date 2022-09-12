3dd08051d0114a3545fe57ea05431f53b438ce72
Roshan Mykoo <roshanjmykoo@gmail.com>
2022-09-12 13:41:30 -0400

    Finally being able to upload this project to git
    I had such an issue trying to upload this project. I usually create the git repo before I even write a line of code but in this instance I thought I was not going to share this project at all. It was such an issue to upload a project that was already created but I found a work around that works for me.

---
+++ Rocial/Rocial/RocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  RocialApp.swift
+//  Rocial
+//
+//  Created by Roshan Mykoo on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct RocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
